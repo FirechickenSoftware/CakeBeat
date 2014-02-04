c4df35734f3180625469e0b92599d7923750f675
waylon531 <waylon531@gmail.com>
2014-02-04 10:20:32 -0800

    Added cutting game, moved level code back to main

--- level1.lua
+++ level1.lua
@@ -1,15 +1,6 @@
 local level1 = {}
 	function level1.update(time,csv,i,y)
-		time2 = love.timer.getTime( ) - time
-		if time2 >= tonumber(csv[i]) then
-			if csv[i+1] ~= nil then
-				i=i+1
-			else
-				level=2
-			end
-			y=10*i
-			--time = love.timer.getTime( )
-		end
-		return y,i
+	end
+	function level1.draw(time,csv,i,y)
 	end
 return level1
--- main.lua
+++ main.lua
@@ -14,6 +14,13 @@ function love.load()
 	--time = math.floor(time)
 	i=1
 	level=1
+	rectangle={}
+	rectangle.x=100
+	rectangle.length=500
+	windowHeight=love.graphics.getHeight()/2
+	mouse={}
+	mouse.x=0
+	mouse.y=0
 end
 function love.keypressed(key, isrepeat)
    if key == 'up' then
@@ -38,7 +45,11 @@ function love.draw()
 		love.graphics.print(time2,0,50)
 		love.graphics.print(csv[i],0,100)
 	elseif level==2 then
-		love.graphics.print("LEVEL 2", 10, 30)
+		love.graphics.print("Cutting", 10, 30)
+		--love.graphics.print(mouse.x..mouse.y,20,40)
+		--love.graphics.print((windowHeight+10)..windowHeight-10,30,60)
+		love.graphics.setColor(255, 0, 0)
+		love.graphics.rectangle( "fill", rectangle.x, windowHeight-10, rectangle.length, 20 )
 	end
 end
 function love.update()
@@ -46,8 +57,26 @@ function love.update()
 	--x=x-width
 	--y=y-height
 	if level==1 then
-		y,i=level1.update(time,csv,i,y)
+		time2 = love.timer.getTime( ) - time
+		if time2 >= tonumber(csv[i]) then
+			if csv[i+1] ~= nil then
+				i=i+1
+			else
+				level=2
+			end
+			y=10*i
+			--time = love.timer.getTime( )
+		end
 	end
 end
 function love.mousepressed()
+	if level==2 then
+		mouse.x, mouse.y = love.mouse.getPosition( )
+		if (mouse.y<(windowHeight+10)) and (mouse.y>(windowHeight-10)) then
+			if (rectangle.length>rectangle.length-mouse.x+rectangle.x) and (0<rectangle.length-mouse.x+rectangle.x) then
+				rectangle.length=rectangle.length-mouse.x+rectangle.x
+				rectangle.x=mouse.x
+			end
+		end	
+	end
 end
